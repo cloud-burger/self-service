6f16ed0c837c94b8827f8050f46519b7147d669f
Daniel Aguiar <daniel_aguiar_alves@hotmail.com>
2024-07-12 20:50:22 -0300

    feat: add tests mocks

---
+++ src/tests/factories/make-customer.ts
@@ -0,0 +1,14 @@
+import { Customer } from '~/domain/customer/entities/customer';
+import { CustomerStatus } from '~/domain/customer/entities/value-objects/enum/customer-status';
+
+export const makeCustomer = (override: Partial<Customer> = {}): Customer =>
+  new Customer({
+    createdAt: new Date('2024-07-12T22:18:26.351Z'),
+    documentNumber: '53523992060',
+    email: 'johndue@gmail.com',
+    id: 'eba521ba-f6b7-46b5-ab5f-dd582495705e',
+    name: 'John Due',
+    status: CustomerStatus.ACTIVE,
+    updatedAt: new Date('2024-07-12T22:18:26.351Z'),
+    ...override,
+  });
