2be81d1a5840009ca95bf7271e8af21f4348cc7f
Daniel Aguiar <daniel_aguiar_alves@hotmail.com>
2024-07-19 14:07:12 -0300

    feat: add order domain

---
+++ src/domain/order/entities/product.ts
@@ -0,0 +1,27 @@
+import { Entity, EntityProps } from '~/domain/core/entities/entity';
+import { ProductCategory } from './value-objects/enums/product-category';
+
+export interface ProductProps extends EntityProps {
+  name: string;
+  category: ProductCategory;
+  description: string;
+  amount: number;
+  image?: any;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export class Product extends Entity {
+  name: string;
+  category: ProductCategory;
+  description: string;
+  amount: number;
+  image?: any;
+  createdAt: Date;
+  updatedAt: Date;
+
+  constructor(input: ProductProps) {
+    super(input.id);
+    Object.assign(this, input);
+  }
+}
---
+++ src/domain/order/entities/value-objects/enums/product-category.ts
@@ -0,0 +1,6 @@
+export enum ProductCategory {
+  BURGER = 'BURGER',
+  SIDE = 'SIDE',
+  DRINK = 'DRINK',
+  DESSERT = 'DESSERT',
+}
