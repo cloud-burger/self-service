96315099e909ac74d58471427c0f48d4ecaa1c40
Gabrielmadrid73 <50714922+Gabrielmadrid73@users.noreply.github.com>
2024-07-29 18:44:21 -0300

    Update src/domain/order/repositories/order.ts
    Co-authored-by: Daniel Alves <daniel_aguiar_alves@hotmail.com>

--- src/domain/order/repositories/order.ts
+++ src/domain/order/repositories/order.ts
@@ -1,5 +1,16 @@
 import { Order } from '../entities/order';
 
+export interface OrderPaginationParams {
+  page: string;
+  size: string;
+  customerId?: string
+  status?: string
+}
+
+export interface OrderRepository {
+  findMany(input: OrderPaginationParams): Promise<Order>;
+}
+
 export interface OrderRepository {
   // create(Order: Order): Promise<void>;
   get(): Promise<Order>;
