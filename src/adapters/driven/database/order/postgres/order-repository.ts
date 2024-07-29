740b03b6937cee7aad4df0543baebd6f474c920d
Gabrielmadrid73 <50714922+Gabrielmadrid73@users.noreply.github.com>
2024-07-29 18:41:51 -0300

    Update src/adapters/driven/database/order/postgres/order-repository.ts
    Co-authored-by: Daniel Alves <daniel_aguiar_alves@hotmail.com>

--- src/adapters/driven/database/order/postgres/order-repository.ts
+++ src/adapters/driven/database/order/postgres/order-repository.ts
@@ -9,7 +9,23 @@ import { GET_ORDERS } from './queries/get-orders';
 export class OrderRepository implements IOrderRepository {
   constructor(private connection: Connection) {}
 
-  async get(): Promise<Order> {
+  async findMany(input: OrderPaginationParams): Promise<Order[]> {
+    const { records } = await this.connection.query({
+      sql: FIND_MANY(input),
+      parameters: { ...input }
+    });
+
+    if (!records.length) {
+      // nesse caso pode só retornar array vazio  
+      return [];
+    }
+
+   
+     //esse cara precisa retornar um array de orders por ser uma listagem  
+     return records.map((record) => {
+      return DatabaseOrderMapper.toDomain(record as OrderDbSchema)
+    });
+  }
     const { records } = await this.connection.query({
       sql: GET_ORDERS,
     });
