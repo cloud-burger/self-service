a2b940c913e80cf046b8fdd93dce7b7a7be7d393
Gabrielmadrid73 <50714922+Gabrielmadrid73@users.noreply.github.com>
2024-07-29 18:41:43 -0300

    Update src/adapters/driven/database/order/postgres/mappers/database-order.ts
    Co-authored-by: Daniel Alves <daniel_aguiar_alves@hotmail.com>

--- src/adapters/driven/database/order/postgres/mappers/database-order.ts
+++ src/adapters/driven/database/order/postgres/mappers/database-order.ts
@@ -3,7 +3,21 @@ import { OrderDbSchema } from '../dtos/order-db-schema';
 
 export class DatabaseOrderMapper {
   static toDomain(orderDbSchema: OrderDbSchema): Order {
+   export class DatabaseOrderMapper {
+  static toDomain(orderDbSchema: OrderDbSchema): Order {
     return new Order({
+      id: orderDbSchema.id,
+      amount: orderDbSchema.amount,
+      customer: orderDbSchema.customer_id ? new Customer({ ...props}) : null
+      status: orderDbSchema.status as OrderStatus,
+      createdAt: orderDbSchema.created_at,
+      updatedAt: orderDbSchema.updated_at,
+      products: orderDbSchema.products.map((product) => {
+        return new Product({...props})
+      }),
+    });
+  }
+}
       id: orderDbSchema.id,
       amount: orderDbSchema.amount,
       customer: orderDbSchema.customer,
