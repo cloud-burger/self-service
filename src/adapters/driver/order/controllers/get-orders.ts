f4ce457a7ed80964862b4c7c9017d14a23c5fb41
Gabrielmadrid73 <50714922+Gabrielmadrid73@users.noreply.github.com>
2024-07-29 18:43:03 -0300

    Update src/adapters/driver/order/controllers/get-orders.ts
    Co-authored-by: Daniel Alves <daniel_aguiar_alves@hotmail.com>

--- src/adapters/driver/order/controllers/get-orders.ts
+++ src/adapters/driver/order/controllers/get-orders.ts
@@ -3,7 +3,30 @@ import logger from '@cloud-burger/logger';
 import { Order } from '~/domain/order/entities/order';
 import { GetOrdersUseCase } from '~/domain/order/use-cases/get-orders';
 
-export class GetOrdersController {
+export class ListOrdersController {
+  constructor(
+    private listOrdersUseCase: ListOrdersUseCase,
+  ) {}
+
+  handler: Controller = async (
+    request: Request,
+  ): Promise<Response<Order>> => {
+
+    logger.info({
+      message: 'List orders request',
+      data: request,
+    });
+
+   // Aqui vale adicionar uma validação dos parâmetros de listagem (pageSize, pageNumber, customerId, status)
+
+    const orders = await this.listOrdersUseCase.execute(..parametros de listagem);
+
+    return {
+      statusCode: 200,
+      body: orders,
+    };
+  };
+}
   constructor(
     private getOrdersUseCase: GetOrdersUseCase,
   ) {}
