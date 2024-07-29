1c7dc2158780ac575d2517df3c813499218407d8
Gabrielmadrid73 <50714922+Gabrielmadrid73@users.noreply.github.com>
2024-07-29 18:41:28 -0300

    Update src/adapters/driven/database/order/postgres/dtos/order-db-schema.ts
    Co-authored-by: Daniel Alves <daniel_aguiar_alves@hotmail.com>

--- src/adapters/driven/database/order/postgres/dtos/order-db-schema.ts
+++ src/adapters/driven/database/order/postgres/dtos/order-db-schema.ts
@@ -1,4 +1,13 @@
-import { Customer } from '~/domain/customer/entities/customer';
+export interface OrderDbSchema {
+  id: string;
+  amount: number;
+  customer_id?: string;
+  customer?: CustomerDbSchema;
+  status: string;
+  created_at: string;
+  updated_at: string;
+  products: ProdcutDbSchema[];
+}
 import { OrderStatus } from '~/domain/order/entities/value-objects/enums/order-status';
 
 export interface OrderDbSchema {
