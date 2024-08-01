2755a27887bef740bb8c90587f8262c3219a4124
Daniel Aguiar <daniel_aguiar_alves@hotmail.com>
2024-08-01 16:19:56 -0300

    feat: add list orders domain layer

--- src/domain/order/repositories/order.ts
+++ src/domain/order/repositories/order.ts
@@ -3,8 +3,7 @@ import { Order } from '../entities/order';
 export interface OrderPaginationParams {
   page: string;
   size: string;
-  customerId?: string
-  status?: string
+  status?: string;
 }
 
 export interface OrderRepository {
