8fc869ac9e43bcfc4f55bcef051a0491ef0dac86
Gabriel Madrid <gabrielmadrid73@gmail.com>
2024-09-10 23:04:43 -0300

    feature/kubernetes - Adjusted infrastructure files

--- infrastructure/aws/eks/default/main.tf
+++ infrastructure/aws/eks/default/main.tf
@@ -47,16 +47,6 @@ locals {
   vpc_cidr = var.vpc_cidr
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
-  gitops_addons_url      = "${var.gitops_addons_org}/${var.gitops_addons_repo}"
-  gitops_addons_basepath = var.gitops_addons_basepath
-  gitops_addons_path     = var.gitops_addons_path
-  gitops_addons_revision = var.gitops_addons_revision
-
-  gitops_workload_url      = "${var.gitops_workload_org}/${var.gitops_workload_repo}"
-  gitops_workload_basepath = var.gitops_workload_basepath
-  gitops_workload_path     = var.gitops_workload_path
-  gitops_workload_revision = var.gitops_workload_revision
-
   aws_addons = {
     enable_cert_manager                          = try(var.addons.enable_cert_manager, false)
     enable_aws_efs_csi_driver                    = try(var.addons.enable_aws_efs_csi_driver, false)
@@ -84,66 +74,10 @@ locals {
     enable_ack_sfn                               = try(var.addons.enable_ack_sfn, false)
     enable_ack_eventbridge                       = try(var.addons.enable_ack_eventbridge, false)
   }
-  oss_addons = {
-    enable_argocd                          = try(var.addons.enable_argocd, true)
-    enable_argo_rollouts                   = try(var.addons.enable_argo_rollouts, false)
-    enable_argo_events                     = try(var.addons.enable_argo_events, false)
-    enable_argo_workflows                  = try(var.addons.enable_argo_workflows, false)
-    enable_cluster_proportional_autoscaler = try(var.addons.enable_cluster_proportional_autoscaler, false)
-    enable_gatekeeper                      = try(var.addons.enable_gatekeeper, false)
-    enable_gpu_operator                    = try(var.addons.enable_gpu_operator, false)
-    enable_ingress_nginx                   = try(var.addons.enable_ingress_nginx, false)
-    enable_kyverno                         = try(var.addons.enable_kyverno, false)
-    enable_kube_prometheus_stack           = try(var.addons.enable_kube_prometheus_stack, false)
-    enable_metrics_server                  = try(var.addons.enable_metrics_server, false)
-    enable_prometheus_adapter              = try(var.addons.enable_prometheus_adapter, false)
-    enable_secrets_store_csi_driver        = try(var.addons.enable_secrets_store_csi_driver, false)
-    enable_vpa                             = try(var.addons.enable_vpa, false)
-  }
-  addons = merge(
-    local.aws_addons,
-    local.oss_addons,
-    { kubernetes_version = local.cluster_version },
-    { aws_cluster_name = module.eks.cluster_name }
-  )
-
-  addons_metadata = merge(
-    module.eks_blueprints_addons.gitops_metadata,
-    {
-      aws_cluster_name = module.eks.cluster_name
-      aws_region       = local.region
-      aws_account_id   = data.aws_caller_identity.current.account_id
-      aws_vpc_id       = module.vpc.vpc_id
-    },
-    {
-      addons_repo_url      = local.gitops_addons_url
-      addons_repo_basepath = local.gitops_addons_basepath
-      addons_repo_path     = local.gitops_addons_path
-      addons_repo_revision = local.gitops_addons_revision
-    },
-    {
-      workload_repo_url      = local.gitops_workload_url
-      workload_repo_basepath = local.gitops_workload_basepath
-      workload_repo_path     = local.gitops_workload_path
-      workload_repo_revision = local.gitops_workload_revision
-    }
-  )
 
   tags = {
     Blueprint  = local.name
     GithubRepo = "github.com/aws-ia/terraform-aws-eks-blueprints"
-  }
-}
-
-################################################################################
-# GitOps Bridge: Bootstrap
-################################################################################
-module "gitops_bridge_bootstrap" {
-  source = "github.com/gitops-bridge-dev/gitops-bridge-argocd-bootstrap-terraform?ref=v2.0.0"
-
-  cluster = {
-    metadata = local.addons_metadata
-    addons   = local.addons
   }
 }
 
@@ -158,9 +92,6 @@ module "eks_blueprints_addons" {
   cluster_endpoint  = module.eks.cluster_endpoint
   cluster_version   = module.eks.cluster_version
   oidc_provider_arn = module.eks.oidc_provider_arn
-
-  # Using GitOps Bridge
-  create_kubernetes_resources = false
 
   # EKS Blueprints Addons
   enable_cert_manager                 = local.aws_addons.enable_cert_manager
@@ -212,14 +143,10 @@ module "eks" {
     coredns    = {}
     kube-proxy = {}
     vpc-cni = {
-      # Specify the VPC CNI addon should be deployed before compute to ensure
-      # the addon is configured before data plane compute resources are created
-      # See README for further details
       before_compute = true
-      most_recent    = true # To ensure access to the latest settings provided
+      most_recent    = true
       configuration_values = jsonencode({
         env = {
-          # Reference docs https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
           ENABLE_PREFIX_DELEGATION = "true"
           WARM_PREFIX_TARGET       = "1"
         }
--- infrastructure/deploy.sh
+++ infrastructure/deploy.sh
@@ -1,8 +1,13 @@
 echo "Deploy EKS cluster!"
-cd aws\eks\default
+cd aws/eks/default
 terraform init
 terraform apply -target="module.vpc" -auto-approve
 terraform apply -target="module.eks" -auto-approve
 terraform apply -auto-approve
-
+aws eks update-kubeconfig --name infra-kubernetes-prd --region us-east-1
+kubectl create ns argocd
+kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml -n argocd
+kubectl port-forward -n argocd svc/argocd-server 8080:80 &
+argocd login localhost:8080 --insecure --username admin --password $(kubectl get secrets argocd-initial-admin-secret -n argocd --template="{{index .data.password | base64decode}}")
+argocd app create self-service --repo https://github.com/cloud-burger/self-service.git --path infrastructure/gitops/argocd/workloads/self-service --dest-server https://kubernetes.default.svc --dest-namespace argocd
 echo "Deploy done!"
