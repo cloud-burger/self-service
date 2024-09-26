3d764181d41ef15a0b914acbb3cce93832f3a2eb
Gabriel Madrid <gabrielmadrid73@gmail.com>
2024-09-25 23:02:54 -0300

    feature/kubernetes - Added argo application sync policy

--- scripts/deploy-aws.sh
+++ scripts/deploy-aws.sh
@@ -15,6 +15,6 @@ sleep 120
 kubectl port-forward -n argocd svc/argocd-server 8080:80 &
 argocd login localhost:8080 --insecure --username admin --password $(kubectl get secrets argocd-initial-admin-secret -n argocd --template="{{index .data.password | base64decode}}")
 echo "Deploying self-service workload!"
-argocd app create self-service --repo https://github.com/cloud-burger/self-service.git --revision feature/kubernetes --path infrastructure/gitops/argocd/self-service --dest-server https://kubernetes.default.svc --dest-namespace argocd --directory-recurse --sync-option ApplyOutOfSyncOnly=true
+argocd app create self-service --repo https://github.com/cloud-burger/self-service.git --revision feature/kubernetes --path infrastructure/gitops/argocd/self-service --dest-server https://kubernetes.default.svc --dest-namespace argocd --directory-recurse --sync-policy automated --sync-option ApplyOutOfSyncOnly=true
 argocd app sync self-service
 echo "Deploy done!"
