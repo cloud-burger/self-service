a645609fad34f563d770d9de8682216d29df4a66
Gabriel Madrid <gabrielmadrid73@gmail.com>
2024-10-07 20:09:48 -0300

    feature/kubernetes - Improved deploy-aws.sh script

--- scripts/deploy-aws.sh
+++ scripts/deploy-aws.sh
@@ -15,4 +15,18 @@ sleep 15
 argocd login localhost:8080 --insecure --username admin --password $(kubectl get secrets argocd-initial-admin-secret -n argocd --template="{{index .data.password | base64decode}}")
 echo "Deploying self-service workload!"
 argocd app create self-service --repo https://github.com/cloud-burger/self-service.git --revision feature/kubernetes --path infrastructure/gitops/argocd/self-service --dest-server https://kubernetes.default.svc --dest-namespace argocd --directory-recurse --sync-policy automated --sync-option ApplyOutOfSyncOnly=true
+kubectl rollout restart deployment istio-ingress -n istio-ingress
+sleep 5
 echo "Deploy done!"
+dns=$(kubectl get svc -n istio-ingress istio-ingress -o jsonpath="{.status.loadBalancer.ingress[*].hostname}")
+echo " 
+################################################################
+################################################################
+
+###  UTILIZE OS DADOS ABAIXO PARA REALIZAR CHAMADAS Á API:
+###  ENDEREÇO: $dns
+###  HOST: api.cloudburger.com.br
+
+################################################################
+################################################################
+"
