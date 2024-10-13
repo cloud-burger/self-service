9406b786bd6b5a2b630a83936b284bec747df04e
Daniel Aguiar <daniel_aguiar_alves@hotmail.com>
2024-10-13 19:18:27 -0300

    chore: change iac folder path

--- scripts/deploy-aws.sh
+++ scripts/deploy-aws.sh
@@ -2,7 +2,7 @@
 set -xe
 
 echo "Deploying EKS cluster!"
-cd ../infrastructure/aws/eks/default
+cd ../iac/aws/eks/default
 terraform init
 terraform apply -auto-approve
 echo "Deploying ArgoCD!"
@@ -14,7 +14,7 @@ kubectl port-forward -n argocd svc/argocd-server 8080:80 > /dev/null 2>&1 &
 sleep 15
 argocd login localhost:8080 --insecure --username admin --password $(kubectl get secrets argocd-initial-admin-secret -n argocd --template="{{index .data.password | base64decode}}")
 echo "Deploying self-service workload!"
-argocd app create self-service --repo https://github.com/cloud-burger/self-service.git --revision main --path infrastructure/gitops/argocd/self-service --dest-server https://kubernetes.default.svc --dest-namespace argocd --directory-recurse --sync-policy automated --sync-option ApplyOutOfSyncOnly=true
+argocd app create self-service --repo https://github.com/cloud-burger/self-service.git --revision main --path iac/gitops/argocd/self-service --dest-server https://kubernetes.default.svc --dest-namespace argocd --directory-recurse --sync-policy automated --sync-option ApplyOutOfSyncOnly=true
 kubectl rollout restart deployment istio-ingress -n istio-ingress
 sleep 5
 echo "Deploy done!"
